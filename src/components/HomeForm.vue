c04cb1030719678550211ef3e366dc29f1f3edfb
Trey Decker <treydecker75@gmail.com>
2019-12-09 20:46:39 -0700

    added logic to add and control the form if empty and refocus after add

--- src/components/HomeForm.vue
+++ src/components/HomeForm.vue
@@ -2,11 +2,34 @@
     <div id="home-form">
         <form @submit.prevent="handleSubmit">
             <label>Home Name</label>
-            <input v-model="home.name" type="text" />
+            <input 
+                ref="first"
+                :class="{ 'has-error': submitting && invalidName }"
+                v-model="home.name" 
+                @focus="clearStatus"
+                @keypress="clearStatus"
+                type="text" 
+            />
             <label>Home Address</label>
-            <input v-model="home.address" type="text" />
+            <input 
+                :class="{ 'has-error': submitting && invalidAddress }"
+                v-model="home.address" 
+                type="text" 
+                @focus="clearStatus"
+            />
             <label>Home Type</label>
-            <input v-model="home.type" type ="text" />
+            <input 
+                :class="{ 'has-error': submitting && invalidType }"
+                v-model="home.type" 
+                type ="text" 
+                @focus="clearStatus"
+            />
+            <p v-if="error && submitting" class="error-message">
+                Please fill out all required fields
+            </p>
+            <p v-if="success" class="success-message">
+                Home added successfully
+            </p>
             <button>Add Home</button>
         </form>
     </div>
@@ -17,6 +40,9 @@ export default {
     name: 'home-form',
     data() {
         return {
+            submitting: false,
+            error: false,
+            success: false,
             home: {
                 name: '',
                 address: '',
@@ -26,14 +52,56 @@ export default {
     },
     methods: {
         handleSubmit() {
+            this.submitting = true
+            this.clearStatus()
+            if (this.invalidName || this.invalidAddress || this.invalidType) {
+                this.error = true
+                return
+            }
             this.$emit('add:home', this.home)
+            this.$refs.first.focus()
+            this.home = { 
+                name: '',
+                address: '',
+                type: '',
+            }
+            this.error = false
+            this.success = false
+            this.submitting = false
         },
+        clearStatus() {
+            this.success = false
+            this.error = false
+        }
     },
+    computed: {
+        invalidName() {
+            return this.home.name === ''
+        },
+        invalidAddress() {
+            return this.home.address === ''
+        },
+        invalidType() {
+            return this.home.type === ''
+        }
+    }
 }
 </script>
 
 <style scoped>
     form {
         margin-bottom: 2rem;
+    }
+
+    [class*='-message'] {
+        font-weight: 500;
+    }
+
+    .error-message {
+        color: #d33c40;
+    }
+
+    .success-message {
+        color: #32a95d;
     }
 </style>
