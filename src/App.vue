6a326ecf790b793f5fb29e3211ef68aeaa2a1cf7
Trey Decker <treydecker75@gmail.com>
2019-12-09 20:33:07 -0700

    lots of changes, now able to add homes. Need to validate forms so they're not empty

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Homes</h1>
+    <home-table :homes="homes"/>
+    <home-form @add:home="addHome" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeTable from '@/components/HomeTable.vue'
+import HomeForm from '@/components/HomeForm.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HomeTable,
+    HomeForm
+  },
+  data() {
+    return {
+      homes: [
+        {
+          id: 1,
+          name: 'Lehigh House',
+          address: '1111 Lehigh',
+          type: 'Main Residence',
+        },
+        {
+          id: 2,
+          name: 'Nort Woods Cabin',
+          address: '1111 Woodsy Lane',
+          type: 'Vacation Home',
+        },
+        {
+          id: 3,
+          name: 'Bachelor Pad',
+          address: '1111 Marmaduke Ave',
+          type: 'Rental Property',
+        },
+      ],
+    }
+  },
+  methods: {
+    addHome(home) {
+      this.homes = [...this.homes, home]
+    }
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
 </style>
