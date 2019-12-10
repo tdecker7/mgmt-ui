86e1e423b0e5ce1f8eb701fe2b795adcd8f8c119
Trey Decker <treydecker75@gmail.com>
2019-12-09 20:33:30 -0700

    added HomeForm and HomeTable vues

---
+++ src/components/HomeForm.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="home-form">
+        <form @submit.prevent="handleSubmit">
+            <label>Home Name</label>
+            <input v-model="home.name" type="text" />
+            <label>Home Address</label>
+            <input v-model="home.address" type="text" />
+            <label>Home Type</label>
+            <input v-model="home.type" type ="text" />
+            <button>Add Home</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home-form',
+    data() {
+        return {
+            home: {
+                name: '',
+                address: '',
+                type: '',
+            },
+        }
+    },
+    methods: {
+        handleSubmit() {
+            this.$emit('add:home', this.home)
+        },
+    },
+}
+</script>
+
+<style scoped>
+    form {
+        margin-bottom: 2rem;
+    }
+</style>
---
+++ src/components/HomeTable.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="home-table">
+        <table>
+            <thead>
+                <tr>
+                    <th>Home Name</th>
+                    <th>Home Address</th>
+                    <th>Home Type</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="home in homes" :key="home.id">
+                    <td>{{ home.name }}</td>
+                    <td>{{ home.address }}</td>
+                    <td>{{ home.type }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home-table',
+        props: {
+            homes: Array,
+        },
+    }
+</script>
+
+<style scoped></style>
